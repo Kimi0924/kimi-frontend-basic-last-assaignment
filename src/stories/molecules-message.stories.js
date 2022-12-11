59ad69d7fef80557dd7076d7aa4cf28e25101de9
Kimi0924 <k.hirokami.22.10.666@4nonome.com>
2022-12-11 16:10:16 +0900

    メッセージ途中まで

---
+++ src/stories/molecules-message.stories.js
@@ -0,0 +1,6 @@
+import Item_Message from "../components/molecules/message/message.html";
+import "../components/molecules/message/message.scss";
+
+export default { title: "molecules/message" };
+
+export const message = () => Item_Message;
